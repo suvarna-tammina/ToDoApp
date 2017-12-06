50fb4b5bd5ad655ca14a9b255b57e3b1f82e882d
Suvarna Bonula <suvarna.bonula@tammina.com>
2017-12-06 11:31:49 +0530

    Unit testing done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,46 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture,TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { AppComponent } from './app.component';
+
+
 describe('AppComponent', () => {
+  let de: DebugElement;
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([
+          { path: '', component: AppComponent }
+        ])
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h1'));
+  });
+  it('should create component', () => expect(comp).toBeDefined() );
+   it(`should have as title "My Sample Application"`,  ()=> {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    expect(comp.title).toEqual('My Sample Application');
+  });
+  it('should display the header text "My Sample Application"', () => {
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    const h1 = de.nativeElement;
+    expect(h1.textContent).toContain('My Sample Application');
+  });
+  
+ 
+  
+ 
+ 
 });
